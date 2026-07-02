719db37115d3eb21086bba4e5c4e595210b1026b
IMarcusDev <escobarmarcos511@gmail.com>
2026-07-02 10:10:29 -0500

    test: added new code to test ci-cd

--- apps/auth-service/src/auth/strategies/local.strategy.ts
+++ apps/auth-service/src/auth/strategies/local.strategy.ts
@@ -14,6 +14,6 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     if (!user) {
       throw new UnauthorizedException('Invalid credentials');
     }
-    return user; // se convierte en req.user
+    return user;
   }
 }
